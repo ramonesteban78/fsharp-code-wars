03fa077c92ed66f20893afc35c4edb82f03dbd11
Ramon Esteban <wantedforcode@outlook.com>
2025-02-27 17:44:52 +0100

    no message

---
+++ src/FSharpCodeWarsTests/SevenKyuTests.fs
@@ -0,0 +1,16 @@
+﻿module Tests
+
+open System
+open Xunit
+
+[<Theory>]
+[<InlineData("123", 123)>] // "123" -> 123
+[<InlineData("a1b2c3", 123)>] // "a1b2c3" -> 123
+[<InlineData("1a2b3c", 123)>] // "1a2b3c" -> 123
+[<InlineData("1a2b3c4", 1234)>] // "1a2b3c4" -> 1234
+[<InlineData("1a2b3c4d5", 12345)>] // "1a2b3c4d5" -> 12345
+let ``filter the Number working fine`` (s: String, expected: int) =
+    let actual = SevenKyu.filterTheNumber s
+    Assert.Equal(expected, actual)
+    let actual2 = SevenKyu.filterTheNumber2 s
+    Assert.Equal(expected, actual2)
